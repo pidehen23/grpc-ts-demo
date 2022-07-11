e185771a9c1680d449651afeafd11b221e80c7c4
chen jiajing <16602132375@163.com>
2022-07-11 23:39:42 +0800

    init

--- gen.sh
+++ gen.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+rm -rf gen
+
+mkdir gen
+
 # 生成 user_pb.js 和 user_grpc_pb.js
-grpc_tools_node_protoc \
+npx grpc_tools_node_protoc \
   --js_out=import_style=commonjs,binary:./gen \
   --grpc_out=grpc_js:./gen \
-  --plugin=protoc-gen-grpc=$(which grpc_tools_node_protoc_plugin) \
+  --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
   ./user.proto
 
 # 生成 d.ts 定义
 protoc \
-  --plugin=protoc-gen-ts=$(which protoc-gen-ts) \
+  --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
   --ts_out=grpc_js:./gen \
   ./user.proto
